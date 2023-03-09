48fd1270a19edfc62552a3efc082a3453bc9533b
Zera tul <max@myplaza.com>
2023-03-09 20:04:03 +0200

    About

--- core/Controller.php
+++ core/Controller.php
@@ -15,48 +15,7 @@ namespace MyApp {
             $this->data['error'] = null;
             $this->data['success'] = null;
             $this->data['message'] = null;
-
-            $this->userAuth = new UserAuthentication();
-
-            $this->formatOptions();
-            $this->formatNavigation();
         }
-
-        protected function formatOptions()
-        {
-            $optModel = new  OptionsModel();
-            $this->data['options'] = [];
-
-            foreach ($optModel->getAllOptions() as $key => $value) {
-                $this->data['options'][$value['name']] = $value;
-            }
-            unset($optModel);
-        }
-
-        protected function formatNavigation()
-        {
-            $navBarModel = new NavigateModel();
-            $this->data['navigate'] = [];
-
-            $result = $navBarModel->getNavigateData();                              // вся навигация
-
-            foreach ($result as $key => $navElem) {
-                if (is_null($navElem['parent_id'])) {                               //если это компонент вверхнего уровня.....
-                    $navElem['childs'] = [];
-                    array_push($this->data['navigate'], $navElem);
-                }
-            }
-
-            foreach ($this->data['navigate'] as $parentInd => $parent) {            //перебираем компоненты верхнего уровня
-                foreach ($result as $childIndex => $child) {                        //перебираем все компоненты
-                    if($child['parent_id'] == $parent["Id"]) {                      //ищем дочерние элементы
-                        array_push($this->data['navigate'][$parentInd]['childs'], $child);
-                    }
-                }
-            }
-            unset($navBarModel);
-        }
-
         public function call($method)
         {
             if (method_exists($this, $method)) {
--- views/header.php
+++ views/header.php
@@ -6,7 +6,7 @@
                 <i class="bi bi-envelope fs-1 text-primary me-3"></i>
                 <div class="text-start">
                     <h6 class="text-uppercase mb-1">Email</h6>
-                    <span><?= $data['options']['email']['value'] ?></span>
+                    <span>email@email.com</span>
                 </div>
             </div>
         </div>
@@ -15,8 +15,8 @@
                 <a href="/" class="navbar-brand">
                     <h1 class="m-0 text-uppercase text-white">
                         <img class="fa fa-birthday-cake logo fs-1 text-dark me-3"
-                             src="<?= $data['options']['logo']['value'] ?>">
-                        <?= $data['options']['title']['value'] ?>
+                             src="/static/img/about.jpg">
+                        Булочная
                     </h1>
                 </a>
             </div>
@@ -26,51 +26,10 @@
                 <i class="bi bi-phone-vibrate fs-1 text-primary me-3"></i>
                 <div class="text-start">
                     <h6 class="text-uppercase mb-1">Связаться с нами</h6>
-                    <span><?= $data['options']['phone']['value'] ?></span>
+                    <span>+3404032905</span>
                 </div>
             </div>
         </div>
     </div>
 </div>
 <!-- Topbar End -->
-
-
-<!-- Navbar Start -->
-<nav class="navbar navbar-expand-lg bg-dark navbar-dark shadow-sm py-3 py-lg-0 px-3 px-lg-0">
-    <a href="index.html" class="navbar-brand d-block d-lg-none">
-        <h1 class="m-0 text-uppercase text-white"><i class="fa fa-birthday-cake fs-1 text-primary me-3"></i>CakeZone
-        </h1>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-        <div class="navbar-nav ms-auto mx-lg-auto py-0">
-            <?php
-            $navigate = $data['navigate'];
-            foreach ($navigate as $key => $value) {
-                if (count($value['childs']) == 0) {
-                    // нету дочерних элементов - не выпадающий список
-                    ?>
-                    <a href="<?= $value['href'] ?>" class="nav-item nav-link active"><?= $value['title'] ?></a>
-                    <?php
-                } else {                    // выпадающий список
-                    ?>
-                    <div class="nav-item dropdown">
-                        <a href="<?= $value['href'] ?>" class="nav-link dropdown-toggle"
-                           data-bs-toggle="dropdown"><?= $value['title'] ?></a>
-                        <div class="dropdown-menu m-0">
-                            <?php
-                            foreach ($value['childs'] as $childIndex => $child) { ?>
-                                <a href="<?= $child['href'] ?>" class="dropdown-item"><?= $child['title'] ?></a>
-                            <?php }
-                            ?>
-                        </div>
-                    </div>
-                <?php }
-            }
-            ?>
-        </div>
-    </div>
-</nav>
-<!-- Navbar End -->
